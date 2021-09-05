9c93c3665b300ba3af1b69adfe5825d4000c507c
Arnab Das <raj713335@gmail.com>
2021-09-05 14:14:08 +0530

    AdvanceCollecible.sol

--- NFTs/contracts/AdvancedCollectible.sol
+++ NFTs/contracts/AdvancedCollectible.sol
@@ -1,40 +1,58 @@
+// SPDX-License-Identifier: MIT
 pragma solidity 0.6.6;
 
-
-import "@openzepplin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
-
 contract AdvancedCollectible is ERC721, VRFConsumerBase {
-
-    bytes32 internal keyHash;
-    uint256 public fee;
-
+    uint256 public tokenCounter;
+    enum Breed{PUG, SHIBA_INU, ST_BERNARD}
+    // add other things
     mapping(bytes32 => address) public requestIdToSender;
     mapping(bytes32 => string) public requestIdToTokenURI;
-    event requestedCollectible(bytes32 indexed requestId);
+    mapping(uint256 => Breed) public tokenIdToBreed;
+    mapping(bytes32 => uint256) public requestIdToTokenId;
+    event requestedCollectible(bytes32 indexed requestId); 
 
 
-    constructor(address _VRFCoordinator, address _LinkToken, bytes32 _keyhash) public
-    VRFConsumer(_VRFCoordinator, _LinkToken)
-    ERC721("Doggies", "Dog")
+    bytes32 internal keyHash;
+    uint256 internal fee;
+    
+    constructor(address _VRFCoordinator, address _LinkToken, bytes32 _keyhash)
+    public 
+    VRFConsumerBase(_VRFCoordinator, _LinkToken)
+    ERC721("Dogie", "DOG")
     {
+        tokenCounter = 0;
         keyHash = _keyhash;
-        fee = 0.1*10**18; // 0.1 LINK 1000000000000000000
+        fee = 0.1 * 10 ** 18;
     }
 
-    function createCollectible(uint256 userProviderSeed, string memory tokenURI) public returns (bytes32) {
-        bytes32 requestId = requestRandomness(keyHash, fee, userProviderSeed);
-        requestIdToSender[requestId] = msg.sender;
-        requestIdToTokenURI[requestId] = tokenURI;
-        emit requestedCollectible(requestId);
+    function createCollectible(string memory tokenURI) 
+        public returns (bytes32){
+            bytes32 requestId = requestRandomness(keyHash, fee);
+            requestIdToSender[requestId] = msg.sender;
+            requestIdToTokenURI[requestId] = tokenURI;
+            emit requestedCollectible(requestId);
     }
 
-
-    function fulfillRandomness(bytes32 requestId, uint256 randomNumber) internal override
-    {
-        
+    function fulfillRandomness(bytes32 requestId, uint256 randomNumber) internal override {
+        address dogOwner = requestIdToSender[requestId];
+        string memory tokenURI = requestIdToTokenURI[requestId];
+        uint256 newItemId = tokenCounter;
+        _safeMint(dogOwner, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        Breed breed = Breed(randomNumber % 3); 
+        tokenIdToBreed[newItemId] = breed;
+        requestIdToTokenId[requestId] = newItemId;
+        tokenCounter = tokenCounter + 1;
     }
 
-
+    function setTokenURI(uint256 tokenId, string memory _tokenURI) public {
+        require(
+            _isApprovedOrOwner(_msgSender(), tokenId),
+            "ERC721: transfer caller is not owner nor approved"
+        );
+        _setTokenURI(tokenId, _tokenURI);
+    }
 }
